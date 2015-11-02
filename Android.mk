52d787717776029ce2814ccf57a1144f019e3274
Legrand Benjamin <android@legrand.ws>
2015-11-02 22:12:43 +0100

    Include only if USE_DEVICE_SPECIFIC_CAMERA is not true

--- Android.mk
+++ Android.mk
@@ -6,8 +6,13 @@ MM_V4L2_DRIVER_LIST += msm_bronze
 MM_V4L2_DRIVER_LIST += msm8916
 MM_V4L2_DRIVER_LIST += msm8909
 LOCAL_32_BIT_ONLY := true
+
+ifneq ($(strip $(USE_DEVICE_SPECIFIC_CAMERA)),true)
+
 ifeq ($(call is-board-platform-in-list,$(MM_V4L2_DRIVER_LIST)),true)
     ifneq ($(BUILD_TINY_ANDROID),true)
       include $(call all-subdir-makefiles)
     endif
+endif
+
 endif
